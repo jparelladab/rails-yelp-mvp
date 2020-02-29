bc69e0bf78293da947e96db67371344a9603e401
joan parellada <joan.parellada789@gmail.com>
2020-02-29 18:06:16 +0100

    bundle and yarn

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create(name: "hoxton 100", address: "hoxton", phone_number: "1234", category: "italian")
+Restaurant.create(name: "dishoom", address: "covent garden", phone_number: "3453", category: "chinese")
+Restaurant.create(name: "bella italia", address: "camden", phone_number: "12343432", category: "italian")
+Restaurant.create(name: "taj mahal", address: "pimlico", phone_number: "157844", category: "belgian")
+Restaurant.create(name: "sushi samba", address: "hyde park", phone_number: "1284764", category: "french")
